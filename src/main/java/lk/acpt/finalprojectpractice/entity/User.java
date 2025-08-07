49aeb76f8b1e277e33314bcd3e22ebe450196f79
Geethanga <dissanayakegeethanga@gmail.com>
2025-08-07 21:04:00 +0530

    add all files to git

---
+++ src/main/java/lk/acpt/finalprojectpractice/entity/User.java
@@ -0,0 +1,20 @@
+package lk.acpt.finalprojectpractice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private int user_id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+}
---
+++ src/test/java/lk/acpt/finalprojectpractice/FinalProjectPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.acpt.finalprojectpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

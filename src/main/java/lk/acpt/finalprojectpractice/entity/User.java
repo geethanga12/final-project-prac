23f14694a5801bdd4407d5412b2fa5805f9c27cd
Geethanga <dissanayakegeethanga@gmail.com>
2025-08-08 01:38:33 +0530

    create user entity and setup database

--- src/main/java/lk/acpt/finalprojectpractice/entity/User.java
+++ src/main/java/lk/acpt/finalprojectpractice/entity/User.java
@@ -2,6 +2,7 @@ package lk.acpt.finalprojectpractice.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int user_id;
     private String username;
     private String password;
